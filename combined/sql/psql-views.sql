cfe66a00df8e37279e3dc9041c815c567b6a0692
caleb rubalema <crubalema@revelooptechsystems.com>
2026-06-24 20:10:40 +0300

    added user_time_spread_view to dw

--- combined/sql/psql-views.sql
+++ combined/sql/psql-views.sql
@@ -1150,3 +1150,26 @@ CREATE INDEX IF NOT EXISTS idx_write_off_trend_account ON write_off_trend_view(c
 CREATE INDEX IF NOT EXISTS idx_write_off_trend_instance_key ON write_off_trend_view(instance_key);
 CREATE INDEX IF NOT EXISTS idx_write_off_trend_received_date ON write_off_trend_view(payment_received);
 CREATE INDEX IF NOT EXISTS idx_write_off_trend_entered_date ON write_off_trend_view(charge_entered_date);
+
+----
+DROP MATERIALIZED VIEW IF EXISTS user_time_spread_view;
+CREATE MATERIALIZED VIEW user_time_spread_view AS
+SELECT
+    uts.customer_account::varchar AS customer_account,
+    uts.instance_key,
+    uts.practice_name,
+    uts.facility_name,
+    uts.office_name,
+    uts.audit_username,
+    uts.audit_action,
+    uts.audit_type,
+    uts.audit_entered_date,
+    uts.audit_entity_id,
+    uts.audit_patient_id::varchar AS audit_patient_id,
+    uts.patient_full_name,
+    uts.created_at
+FROM user_time_spread uts;
+
+CREATE INDEX IF NOT EXISTS idx_user_time_spread_account ON user_time_spread_view(customer_account);
+CREATE INDEX IF NOT EXISTS idx_user_time_spread_instance_key ON user_time_spread_view(instance_key);
+CREATE INDEX IF NOT EXISTS idx_user_time_spread_username ON user_time_spread_view(audit_username);
